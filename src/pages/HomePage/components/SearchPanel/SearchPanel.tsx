bb836d5e2b18272cb12db87a8e91957b5f3b4058
Julia Lepner <ylepner@gmail.com>
2023-10-27 14:03:42 +0200

    feat: save search term to local storage

--- src/pages/HomePage/components/SearchPanel/SearchPanel.tsx
+++ src/pages/HomePage/components/SearchPanel/SearchPanel.tsx
@@ -1,14 +1,29 @@
-import { Component, createRef } from 'react';
+import { ChangeEvent, Component, createRef } from 'react';
 
 interface SearchPanelProps {
   onSearch: (searchTerm: string) => void;
 }
-
-class SearchPanel extends Component<SearchPanelProps> {
+class SearchPanel extends Component<SearchPanelProps, { value: string }> {
   private inputRef = createRef<HTMLInputElement>();
   constructor(props: SearchPanelProps) {
     super(props);
+    this.state = {
+      value: localStorage.getItem('searchTerm') || '',
+    };
   }
+
+  componentWillUnmount() {
+    this.saveToStorage(this.state.value);
+  }
+
+  saveToStorage(searchTerm: string) {
+    localStorage.setItem('searchTerm', searchTerm);
+  }
+
+  handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ value: e.target.value });
+    this.saveToStorage(e.target.value);
+  };
 
   handleSearchTermChange = () => {
     const searchTerm = this.inputRef.current?.value ?? '';
@@ -25,6 +40,8 @@ class SearchPanel extends Component<SearchPanelProps> {
             type="text"
             placeholder="What book do you search for?"
             ref={this.inputRef}
+            value={this.state.value}
+            onChange={this.handleSearchChange}
           />
           <button onClick={this.handleSearchTermChange}>Search</button>
         </div>
