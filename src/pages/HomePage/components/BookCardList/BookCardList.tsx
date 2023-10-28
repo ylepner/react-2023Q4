a86b15a5710c9b2e86ee01b82883f35eaf0fbcd1
Julia Lepner <ylepner@gmail.com>
2023-10-28 22:55:06 +0200

    style: style book list

--- src/pages/HomePage/components/BookCardList/BookCardList.tsx
+++ src/pages/HomePage/components/BookCardList/BookCardList.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import BookCard from '../BookCard/BookCard';
 import { BookInfo } from '../BookCard/models';
+import './BookCardList.css';
 
 interface BookCardListProps {
   books: BookInfo[];
@@ -13,11 +14,13 @@ class BookCardList extends Component<BookCardListProps> {
 
   render() {
     return (
-      <div className="card-list">
-        Found: {this.props.books.length}
-        {this.props.books.map((book) => (
-          <BookCard key={book.key} book={book} />
-        ))}
+      <div>
+        <div className="text-center p-5">Results: {this.props.books.length}</div>
+        <div className="grid grid-rows-2 grid-flow-col gap-4">
+          {this.props.books.map((book) => (
+            <BookCard key={book.key} book={book} />
+          ))}
+        </div>
       </div>
     );
   }
