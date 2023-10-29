ec0997530c8212b170d9aabe07156c729534de1a
Julia Lepner <ylepner@gmail.com>
2023-10-29 13:55:38 +0100

    style: style header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header className="wrapper">
-        <h1>Header</h1>
+      <header className="wrapper pt-12 pb-16 flex justify-between">
+        <div className="flex">
+          <img className="pr-3" src="./src/assets/BookOpen.svg" alt="book icon" />
+          <h3>Open Library</h3>
+        </div>
+        <button>
+          <img src="./src/assets/BOOKMARK_SIMPLE.svg" alt="bookmark icon" />
+        </button>
       </header>
     );
   }
