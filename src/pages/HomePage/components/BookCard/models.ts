28800be33483b474c529e68525dd2dd2ed674159
Julia Lepner <ylepner@gmail.com>
2023-10-31 15:08:07 +0100

    feat: update book models

--- src/pages/HomePage/components/BookCard/models.ts
+++ src/pages/HomePage/components/BookCard/models.ts
@@ -7,10 +7,43 @@ export interface BooksResponse {
   q: string;
   offset: null;
 }
-
 export interface BookInfo {
   key: string;
   title: string;
   cover_i?: number;
   author_name: string[];
+}
+export interface BookDetails {
+  description: string;
+  title: string;
+  covers: number[];
+  first_sentence: Created;
+  first_publish_date: string;
+  key: string;
+  authors: Author[];
+  excerpts: Excerpt[];
+  type: Type;
+  subjects: string[];
+  latest_revision: number;
+  revision: number;
+  created: Created;
+  last_modified: Created;
+}
+
+export interface Author {
+  author: Type;
+  type: Type;
+}
+
+export interface Type {
+  key: string;
+}
+
+export interface Created {
+  type: string;
+  value: string;
+}
+
+export interface Excerpt {
+  excerpt: string;
 }
