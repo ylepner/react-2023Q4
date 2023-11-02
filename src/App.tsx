d67e29078c937701f13e8e659d2a34318ea21df8
Julia Lepner <ylepner@gmail.com>
2023-11-02 12:01:52 +0100

    feat: add nested root

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { Route, Routes } from 'react-router';
 import BookCardDetails from './pages/HomePage/components/BookCardDetails/BookCardDetails';
-import HomePage from './pages/HomePage/HomePage';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { useParams } from 'react-router-dom';
+import SearchResults from './pages/SearchResults/SearchResults';
+
+function BindSearchResult() {
+  const { searchTerm } = useParams();
+  return <SearchResult searchTerm={searchTerm ?? ''} />;
+}
+
+function SearchResult(props: { searchTerm: string }) {
+  return <div>Search result for: {props.searchTerm}</div>;
+}
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/details/works/:id" element={<BookCardDetails />} />
+        <Route path="/" element={<SearchResults />} />
+        <Route path="/search/:searchTerm" element={<BindSearchResult />}>
+          <Route
+            path="details/:id"
+            element={<BookCardDetails id={'OL35351151W'} />}
+          ></Route>
+        </Route>
       </Routes>
       <Footer />
     </>
